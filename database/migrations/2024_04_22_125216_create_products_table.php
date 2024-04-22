aaa8d0c237e66cfb498e9e7234bf8f117093ec63
Thomaz <thomaz2pinheiro@gmail.com>
2024-04-22 10:33:35 -0300

    create_products migration fix

--- database/migrations/2024_04_22_125216_create_products_table.php
+++ database/migrations/2024_04_22_125216_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->double('price');
+            $table->string('img_url');
             $table->timestamps();
         });
     }
